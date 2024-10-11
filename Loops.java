23b0e1cd11fc3a79de28e300b72cb92d4f1879f0
Abir Shaikh <shaikhabir044@gmail.com>
2024-10-11 17:20:12 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Loops {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 10; num++) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ greeting.java
@@ -0,0 +1,11 @@
+
+
+import java.util.Scanner;
+public class greeting {
+    public static void main(String[] args) {
+        String personal = mygreet("abir shaikh");
+    }
+    static String mygreet(String name){
+        return mygreet("abir shaikh");
+    }
+}
