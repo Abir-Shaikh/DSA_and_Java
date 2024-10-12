6eead6cd6a9fbdc709c7611857978c52a583a643
Abir Shaikh <shaikhabir044@gmail.com>
2024-10-12 20:20:59 +0530

    Add files via upload

---
+++ functions and methods/funcEvenOdd.java
@@ -0,0 +1,14 @@
+//Define a program to find out whether a given number is even or odd.
+
+import java.util.Scanner;
+public class funcEvenOdd {
+    public static void main(String[] args) {
+        int a = 135;
+        if (a % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
+
