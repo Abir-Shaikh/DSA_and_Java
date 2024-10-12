74f96eb875808564ffba0f85baaa357a3588d752
Abir Shaikh <shaikhabir044@gmail.com>
2024-10-12 11:51:08 +0530

    Delete Loops.java

--- Loops.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class Loops {
-    public static void main(String[] args) {
-        for (int num = 1; num <= 10; num++) {
-            System.out.println(num);
-        }
-    }
-}
